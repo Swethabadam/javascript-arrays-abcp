564245a78fc4d0345c0dd01fc7985c532776dcb9
SWETHA BADAM <sweth.badam@gmail.com>
2018-03-08 22:33:12 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,9 @@
+function addElementToBeginningOfArray(array,element){
+  var array=["snickers","hundred grand","kitkat","skittles"];
+  array.push(element);
+}
+addElementToBeginningOfArray(chocolates,dairymilk);
+/*function destructivelyAddElementToBeginningOfArray(array,element){
+  
+  
+}*/
